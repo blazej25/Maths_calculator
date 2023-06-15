b97c6326edc35f05a13ad80c6040a1f626fec4be
bkubica25 <bkubica25@gmail.com>
2023-06-15 22:06:54 +0200

    Expanding binomials added

--- Maths_calculator.py
+++ Maths_calculator.py
@@ -1,4 +1,8 @@
 import PySimpleGUI as sg 
+sg.theme('DarkAmber')
+
+def getLabelledInputBox(box_name, tag_name):
+    return [sg.Text(f'{box_name}: ', size = (15, 1)), sg.Input(key = f'-{tag_name}-')]
 
 def factorial(n):
     match n:
@@ -18,27 +22,46 @@ def combinations(total, choose):
 
     result = f_total / (f_choose * f_diff)
 
-    return result 
+    return int(result)
 
-def getLabelledInputBox(box_name, tag_name):
-    return [sg.Text(f'{box_name}: '), sg.Input(f'-{tag_name}-')]
+def bin_expand(a, b, pow):
+    result = ''
 
+    for i in range(pow + 1):
+        if pow - i == 0:
+            result += f'+ {b**pow}'
+        elif i == 0:
+            result += f'{a**pow}x^{pow} '
+        else:
+            result += f'+ {(combinations(pow, i) * (b**i) * a**(pow - i))}x^{pow - i} '
+
+    return result
+    
 def gui():
     layout1 = [[sg.Text('Welcome to our maths calculator! What would you like to calculate?')],
-               [sg.Button('Combinations')],
+               [sg.Button('Combinations'), sg.Button('Binomial expansion')],
                [],
                [sg.Button('Exit')]]
 
     layout2 = [[sg.Text('Calculating combinations. Your result: '), sg.Text(key = '-COM-')],
-               [sg.Text('Total: '), sg.Input(key = '-TOT-')],
-               [sg.Text('Choose: '), sg.Input(key = '-CHO-')],
+               getLabelledInputBox('Total', 'TOT'),
+               getLabelledInputBox('Choose', 'CHO'),
                [sg.Button('Calculate', key = '-C1-')],
-               [], [sg.Button('Back')]] 
+               [sg.Button('Back')]] 
 
-    layout = [[sg.Column(layout1, key = '-DEF-'), sg.Column(layout2, key = '-COL2-', visible = False)]]
+    layout3 = [[sg.Text('Expanding a binomial.')],
+               getLabelledInputBox('a', 'A'),
+               getLabelledInputBox('b', 'B'),
+               getLabelledInputBox('Power', 'POW'),
+               [sg.Button('Calculate', key = '-C2-'), sg.Button('Back')], 
+               [sg.Text('The binomial expansion is: '), sg.Text(key = '-BIN-')]]
+               
+    
+    layout = [[sg.Column(layout1, key = '-DEF-'), sg.Column(layout2, key = '-COL2-', visible = False), sg.Column(layout3, key = '-COL3-', visible = False)]]
 
-    window = sg.Window('Maths Calculator', layout)
+    window = sg.Window('WolframSigma', layout)
 
+    curr_layout  = 1
     while True:
         event, values = window.read()
         print(event, values)
@@ -49,13 +72,26 @@ def gui():
         if event == 'Combinations':
             window['-DEF-'].update(visible = False)
             window['-COL2-'].update(visible = True)
+            curr_layout = 2 
 
         if event == '-C1-':
-            window['-COM-'].update(combinations(int(values['-TOT-']), int(values['-CHO-'])))
+            if int(values['-CHO-']) > int(values['-TOT-']):
+                sg.popup('Total has to be greater than choose!!!')
+            else:
+                window['-COM-'].update(combinations(int(values['-TOT-']), int(values['-CHO-'])))
         
-        if event == 'Back': 
-            window['-COL2-'].update(visible = False)
+        if 'Back' in event: 
+            window[f'-COL{curr_layout}-'].update(visible = False)
             window['-DEF-'].update(visible = True)
+            curr_layout = 1 
+
+        if event == 'Binomial expansion':
+            window['-DEF-'].update(visible = False)
+            window['-COL3-'].update(visible = True)
+            curr_layout = 3
+
+        if event == '-C2-':
+            window['-BIN-'].update(bin_expand(int(values['-A-']), int(values['-B-']), int(values['-POW-'])))
 
 if __name__ == '__main__':
     gui()
